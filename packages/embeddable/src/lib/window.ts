1fca67a46a0faf801f939bc3a2c69d89028c8ed2
Josue Montano <josuemontanoa@gmail.com>
2020-10-22 16:38:06 -0400

    Create the useFrameSize hook

---
+++ packages/embeddable/src/lib/window.ts
@@ -0,0 +1,27 @@
+import { useMemo } from 'preact/hooks';
+import { useWindowSize } from 'react-use';
+
+/**
+ * Hook to resize an iframe when the viewport changes
+ *
+ * @example
+ * const { width, height } = useFrameSize(100, 500)
+ */
+export function useFrameSize(width?: number, height?: number) {
+  const windowSize = useWindowSize();
+  const frameMargin = 20;
+
+  // Returns the max allowed width minus a margin
+  const maxWidth = useMemo(() => {
+    const w = width ? Math.min(windowSize.width, width) : windowSize.width;
+    return w - frameMargin;
+  }, [windowSize.width, width]);
+
+  // Returns the max allowed height minus a margin
+  const maxHeight = useMemo(() => {
+    const h = height ? Math.min(windowSize.height, height) : windowSize.height;
+    return h - frameMargin;
+  }, [windowSize.height, height]);
+
+  return { width: maxWidth, height: maxHeight };
+}
