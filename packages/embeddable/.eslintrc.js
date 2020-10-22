a286a29da8dd4938ee91ad0956b08d4f6cb6ce95
Josue Montano <josuemontanoa@gmail.com>
2020-10-22 07:41:24 -0400

    Add cypress plugin for eslint

--- packages/embeddable/.eslintrc.js
+++ packages/embeddable/.eslintrc.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'cypress'],
   extends: [
     'eslint:recommended',
     'preact',
@@ -11,4 +11,7 @@ module.exports = {
     'prettier/react',
   ],
   rules: { '@typescript-eslint/explicit-module-boundary-types': 0 },
+  env: {
+    'cypress/globals': true,
+  },
 };
