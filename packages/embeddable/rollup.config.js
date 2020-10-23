ffebebd3e2ee5eb9ca5343fd4ba0b9dd378edd6d
Josue Montano <josuemontanoa@gmail.com>
2020-10-23 08:05:56 -0400

    Fix building

--- packages/embeddable/rollup.config.js
+++ packages/embeddable/rollup.config.js
@@ -1,37 +1,44 @@
-import alias from '@rollup/plugin-alias';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import strip from '@rollup/plugin-strip';
 import license from 'rollup-plugin-license';
+import serve from 'rollup-plugin-serve';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default [
   {
     input: 'src/index.tsx',
+    context: 'this',
     plugins: [
-      alias({
-        entries: {
-          react: 'preact/compat',
-          'react-dom': 'preact/compat',
-        },
-      }),
+      nodeResolve({ browser: true }),
       typescript({
+        rollupCommonJSResolveHack: true,
         useTsconfigDeclarationDir: true,
+        clean: true,
       }),
-      strip(),
+      commonjs({
+        include: ['node_modules/**'],
+      }),
       replace({
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       }),
-      terser(),
-      license({
-        banner: `MagicBell JavaScript Library <%= pkg.version %>\nhttps://magicbell.io\nCopyright <%= new Date().getFullYear() %>, SupportBee`,
-      }),
+      !isProduction && serve({ contentBase: ['public', 'dist'] }),
+      isProduction && strip(),
+      isProduction && terser(),
+      isProduction &&
+        license({
+          banner: `MagicBell JavaScript Library <%= pkg.version %>\nhttps://magicbell.io\nCopyright <%= new Date().getFullYear() %>, SupportBee`,
+        }),
     ],
     output: [
       {
         name: 'MagicBell',
-        file: 'dist/widget.js',
-        format: 'umd',
+        file: 'dist/magicbell-js.min.js',
+        format: 'iife',
       },
     ],
   },
