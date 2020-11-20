f7f8cb0b2b99d5b97db2a7c1d1c23d56ef62893f
Josue Montano <josuemontanoa@gmail.com>
2020-11-20 00:19:22 -0400

    Add missing specs

---
+++ packages/embeddable/cypress/integration/widget.spec.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+context('Widget', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route('GET', 'https://api.magicbell.io/notifications**', 'fx:notifications.json').as('getNotifications');
+
+    cy.visit('/');
+  });
+
+  it('parses the script', () => {
+    cy.window().its('magicbell').should('be.a', 'function');
+  });
+
+  it('opens the notification inbox when the container is clicked', () => {
+    cy.get('#notifications').click();
+
+    cy.get('iframe[id="magicbell-frame"]').should('be.visible');
+    cy.get('iframe[id="magicbell-frame"]')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .should('contain', 'Notifications')
+      .should('contain', 'Mark All Read')
+      .should('contain', 'Powered by');
+  });
+
+  context('unseen badge', () => {
+    it('shows the badge by default', () => {
+      cy.get('[data-testid="bell"]').should('contain.text', '1');
+    });
+
+    it('hides the badge on click', () => {
+      cy.get('[data-testid="bell"]').click();
+      cy.get('[data-testid="bell"]').should('not.contain.text', '1');
+    });
+  });
+
+  context('click outside', () => {
+    it('closes the notification inbox', () => {
+      cy.get('[data-testid="bell"]').click();
+
+      cy.get('iframe[id="magicbell-frame"]').should('be.visible');
+      cy.contains('Quick Start').click();
+      cy.get('iframe[id="magicbell-frame"]').should('not.be.visible');
+    });
+  });
+
+  context('click a notification', () => {
+    it('opens the notification url ', () => {
+      cy.get('[data-testid="bell"]').click();
+      cy.get('iframe[id="magicbell-frame"]')
+        .its('0.contentDocument.body')
+        .should('not.be.empty')
+        .contains('Board View')
+        .click();
+
+      cy.url().should('eq', 'http://localhost:10001/#developers');
+    });
+  });
+});
