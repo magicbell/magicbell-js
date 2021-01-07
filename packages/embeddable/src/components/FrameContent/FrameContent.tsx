8ce641432feb18b3d28e5ae73305d24718bd8417
Josue Montano <josuemontanoa@gmail.com>
2021-01-07 10:22:09 -0400

    Report unhandled errors to rollbar

--- packages/embeddable/src/components/FrameContent/FrameContent.tsx
+++ packages/embeddable/src/components/FrameContent/FrameContent.tsx
@@ -1,7 +1,7 @@
 import { NotificationInbox } from '@magicbell/magicbell-react';
 import { NotificationInboxProps } from '@magicbell/magicbell-react/dist/components/NotificationInbox';
 import React, { Component } from 'react';
-import { reportReactError } from '../../lib/error';
+import { reportReactError, reportUnhandledError } from '../../lib/error';
 
 export type FrameContentProps = NotificationInboxProps;
 
@@ -14,6 +14,14 @@ export type FrameContentProps = NotificationInboxProps;
 export default class FrameContent extends Component<FrameContentProps> {
   componentDidCatch(error: Error, info: React.ErrorInfo) {
     reportReactError(error, info);
+  }
+
+  componentDidMount() {
+    window.onerror = function (message, source, lineno, colno, error) {
+      reportUnhandledError(error);
+      // Prevents the firing of the default event handler
+      return true;
+    };
   }
 
   render() {
