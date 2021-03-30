534b36c89a43e2aa75edf517d93b75136a62cdda
Josue Montano <josuemontanoa@gmail.com>
2021-03-30 17:11:34 -0400

    Update eslint prettier config

--- packages/embeddable/.eslintrc.js
+++ packages/embeddable/.eslintrc.js
@@ -2,14 +2,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'cypress'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   rules: { '@typescript-eslint/explicit-module-boundary-types': 0 },
   env: {
     'cypress/globals': true,
