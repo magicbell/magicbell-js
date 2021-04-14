1c965a32ed260574bf409ec16d2cbc7a48385662
Josue Montano <josuemontanoa@gmail.com>
2021-04-13 20:56:50 -0400

    Implement the findByIndex method

--- packages/embeddable/src/web_push_notifications/db.ts
+++ packages/embeddable/src/web_push_notifications/db.ts
@@ -19,8 +19,8 @@ export async function getDB() {
 /**
  * Save an object in the MagicBell's IndexedDB database.
  *
- * @param obj - Object to store
- * @param storeName - Name of the IndexedDB object store
+ * @param obj Object to store
+ * @param storeName Name of the IndexedDB object store
  */
 export async function save(obj, storeName: string) {
   const db = await getDB();
@@ -28,13 +28,26 @@ export async function save(obj, storeName: string) {
 }
 
 /**
- * Fetch an object from the MagicBell's IndexedDB database.
+ * Fetch an object from the MagicBell's IndexedDB database by ID.
  *
- * @param id - ID of the object to retrieve
- * @param storeName - Name of the IndexedDB object store
+ * @param id ID of the object to retrieve
+ * @param storeName Name of the IndexedDB object store
  */
 export async function find(id: number | string, storeName: string) {
   const db = await getDB();
   const obj = await db.get(storeName, id);
+  return obj || null;
+}
+
+/**
+ * Fetch an object from the MagicBell's IndexedDB database by index.
+ *
+ * @param value Value
+ * @param storeName Name of the IndexedDB object store
+ * @param indexName Name of the IndexedDB index
+ */
+export async function findByIndex(value: number | string, storeName: string, indexName: string) {
+  const db = await getDB();
+  const obj = await db.getFromIndex(storeName, indexName, value);
   return obj || null;
 }
