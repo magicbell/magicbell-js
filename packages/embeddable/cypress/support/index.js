6562d382e33a1a8ac544f191f0be5a12229d55dd
Josue Montano <josuemontanoa@gmail.com>
2021-08-17 16:22:07 -0400

    Ignore unhandled promise rejections

--- packages/embeddable/cypress/support/index.js
+++ packages/embeddable/cypress/support/index.js
@@ -14,7 +14,18 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable, promise) => {
+  // when the exception originated from an unhandled promise
+  // rejection, the promise is provided as a third argument
+  // you can turn off failing the test in this case
+  if (promise) {
+    return false;
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+});
