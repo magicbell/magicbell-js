b662a84cd41aa1a26c32fcaab8f84db781c5ecea
Josue Montano <josuemontanoa@gmail.com>
2021-09-10 00:59:19 -0400

    Fix linting of TS files

--- packages/react-headless/.eslintrc.js
+++ packages/react-headless/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  extends: ['prettier', 'plugin:prettier/recommended'],
   plugins: ['@typescript-eslint'],
 };
