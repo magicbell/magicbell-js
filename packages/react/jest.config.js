fd1eedbc616222d126911192d51e9b77633c5a58
Josue Montano <josuemontanoa@gmail.com>
2021-09-11 12:51:45 -0400

    Add set up file for jest

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   coverageDirectory: '<rootDir>/coverage',
+  setupFilesAfterEnv: ['./tests/setupTests.ts'],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
   snapshotSerializers: ['@emotion/jest/serializer'],
   coveragePathIgnorePatterns: ['/node_modules/', 'tests/'],
---
+++ packages/react/tests/setupTests.ts
@@ -0,0 +1,1 @@
+global.open = jest.fn();
