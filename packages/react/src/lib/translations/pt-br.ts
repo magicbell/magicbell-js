0825395dc916c2d7a7a1b1f2a846c3da37ac9d5f
Josue Montano <josuemontanoa@gmail.com>
2021-09-11 12:48:47 -0400

    Add pt_BR (Brazilian Protuguese) locale file

---
+++ packages/react/src/lib/translations/pt-br.ts
@@ -0,0 +1,20 @@
+export default {
+  header: {
+    title: 'NOTIFICAÇÕES',
+    'mark-all-read': 'Marcar tudo como lido',
+  },
+  notification: {
+    'mark-as-read': 'Marcar como lido',
+    'mark-as-unread': 'Marcar como não lido',
+    delete: 'Apagar',
+  },
+  'web-push-notifications': {
+    notice: 'Ao ativar as notificações do navegador, você ficará ainda melhor atualizado.',
+    'enable-now': 'Ativar',
+  },
+  messages: {
+    'empty-inbox': 'Nenhuma notificação disponível!',
+    'server-error': 'Não conseguimos recuperar suas notificações.<br>Por favor, volte em breve.',
+    'no-internet-connection': 'Não conseguimos nos conectar à Internet.<br>Por favor, verifique sua conexão.',
+  },
+};
