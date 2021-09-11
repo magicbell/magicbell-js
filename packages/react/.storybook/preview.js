4d5dfd83c92e8c8378dda7461bb694b8fd7243c9
Josue Montano <josuemontanoa@gmail.com>
2021-09-10 23:44:38 -0400

    Initialize storybook

---
+++ packages/react/.storybook/preview.js
@@ -0,0 +1,10 @@
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: 'centered',
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
