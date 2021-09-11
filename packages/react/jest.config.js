7ed88e375e25fc4389d5d40b08e08451c784592b
Josue Montano <josuemontanoa@gmail.com>
2021-09-10 23:55:32 -0400

    Add emotion serializer

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   coverageDirectory: '<rootDir>/coverage',
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  snapshotSerializers: ['@emotion/jest/serializer'],
   coveragePathIgnorePatterns: ['/node_modules/', 'tests/'],
   restoreMocks: true,
 };
