f68a70d40d75d775b393fc3072a9f0f45728488c
Josue Montano <josuemontanoa@gmail.com>
2021-09-11 12:48:12 -0400

    Add Spanish locale file

---
+++ packages/react/src/lib/translations/es.ts
@@ -0,0 +1,18 @@
+export default {
+  header: {
+    title: 'NOTIFICACIONES',
+    'mark-all-read': 'Marcar todo como leído',
+  },
+  notification: {
+    'mark-as-read': 'Marcar como leído',
+    'mark-as-unread': 'Marcar como no leído',
+    delete: 'Eliminar',
+  },
+  'web-push-notifications': {
+    notice: 'Active notificaciones en su navegador para mantenerse actualizado',
+    'enable-now': 'Activar',
+  },
+  messages: {
+    'empty-inbox': '¡No tiene notificaciones!<br>Le haremos saber cuando llegue una.',
+  },
+};
