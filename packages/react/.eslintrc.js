c4d53b3032a46af9b39a17974a0fd22c1aefdafb
Josue Montano <josuemontanoa@gmail.com>
2021-09-10 22:28:31 -0400

    Add eslint configuration file

---
+++ packages/react/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['react-app', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+};
