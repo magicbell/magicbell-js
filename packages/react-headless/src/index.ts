aa91df7eaa5c4598b9959d4923a72b8bddc22b28
Josue Montano <josuemontanoa@gmail.com>
2021-09-13 08:29:53 -0400

    Export stores

--- packages/react-headless/src/index.ts
+++ packages/react-headless/src/index.ts
@@ -7,3 +7,6 @@ export { default as useNotificationFactory } from './hooks/useNotificationFactor
 export { default as useNotifications } from './hooks/useNotifications';
 export { default as useNotificationUnmount } from './hooks/useNotificationUnmount';
 export { secondsToDate, toDate, toUnix } from './lib/date';
+export { default as clientSettings } from './stores/clientSettings';
+export { default as useConfig } from './stores/config';
+export { useNotificationStoresCollection } from './stores/notifications';
