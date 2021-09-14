526738b533934243b8bd0bf128364c4d5185d9db
Josue Montano <josuemontanoa@gmail.com>
2021-09-14 01:46:46 -0400

    Use classic runtime

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,4 +1,15 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  babel: async (options) => {
+    return {
+      ...options,
+      presets: [
+        ['@babel/preset-env', { shippedProposals: true, loose: true }],
+        '@babel/preset-typescript',
+        ['@babel/preset-react', { runtime: 'classic' }],
+        '@babel/preset-flow',
+      ],
+    };
+  },
 };
