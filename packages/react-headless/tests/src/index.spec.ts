6d5722a2738a83d6c172b309ca4a0fa82b764c28
Josue Montano <josuemontanoa@gmail.com>
2021-09-13 22:01:10 -0400

    Improve code cov

---
+++ packages/react-headless/tests/src/index.spec.ts
@@ -0,0 +1,79 @@
+import {
+  clientSettings,
+  deleteAPI,
+  fetchAPI,
+  MagicBellProvider,
+  postAPI,
+  putAPI,
+  RealtimeListener,
+  secondsToDate,
+  toDate,
+  toUnix,
+  useBell,
+  useNotification,
+  useNotifications,
+  useNotificationStoresCollection,
+  useNotificationUnmount,
+} from '../../src';
+
+describe('index.ts', () => {
+  it('exports the clientSettings store', () => {
+    expect(clientSettings).toBeDefined();
+  });
+
+  it('exports the useBell hook', () => {
+    expect(useBell).toBeDefined();
+  });
+
+  it('exports the useNotification hook', () => {
+    expect(useNotification).toBeDefined();
+  });
+
+  it('exports the useNotifications hook', () => {
+    expect(useNotifications).toBeDefined();
+  });
+
+  it('exports the useNotificationStoresCollection hook', () => {
+    expect(useNotificationStoresCollection).toBeDefined();
+  });
+
+  it('exports the useNotificationUnmount hook', () => {
+    expect(useNotificationUnmount).toBeDefined();
+  });
+
+  it('exports the MagicBellProvider component', () => {
+    expect(MagicBellProvider).toBeDefined();
+  });
+
+  it('exports the RealtimeListener component', () => {
+    expect(RealtimeListener).toBeDefined();
+  });
+
+  it('exports the deleteAPI function', () => {
+    expect(deleteAPI).toBeDefined();
+  });
+
+  it('exports the fetchAPI function', () => {
+    expect(fetchAPI).toBeDefined();
+  });
+
+  it('exports the postAPI function', () => {
+    expect(postAPI).toBeDefined();
+  });
+
+  it('exports the putAPI function', () => {
+    expect(putAPI).toBeDefined();
+  });
+
+  it('exports the toDate function', () => {
+    expect(toDate).toBeDefined();
+  });
+
+  it('exports the toUnix function', () => {
+    expect(toUnix).toBeDefined();
+  });
+
+  it('exports the secondsToDate function', () => {
+    expect(secondsToDate).toBeDefined();
+  });
+});
