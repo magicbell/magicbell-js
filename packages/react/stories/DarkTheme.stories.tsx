ff9dfa5da7d8cd4c798233783b95a0d2ab4b3cfd
Josue Montano <josuemontanoa@gmail.com>
2021-09-19 23:30:46 -0400

    Add stories

---
+++ packages/react/stories/DarkTheme.stories.tsx
@@ -0,0 +1,88 @@
+import { INotification } from '@magicbell/react-headless';
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import FloatingNotificationInbox from '../src/components/FloatingNotificationInbox';
+import MagicBell, { MagicBellProps } from '../src/components/MagicBell';
+import { IMagicBellTheme } from '../src/context/Theme';
+import { DeepPartial } from '../src/lib/types';
+
+interface IStory extends MagicBellProps {
+  onAllRead: () => void;
+  onNotificationClick: (notification: INotification) => void;
+  theme: DeepPartial<IMagicBellTheme>;
+}
+
+export default {
+  title: 'Examples/Dark theme',
+  component: MagicBell,
+  argTypes: {
+    onAllRead: { action: 'onAllRead' },
+    onNotificationClick: { action: 'onNotificationClick' },
+  },
+} as Meta;
+
+const Template: Story<IStory> = ({ onAllRead, onNotificationClick, ...args }: IStory) => (
+  <div
+    style={{
+      height: '600px',
+      width: '1200px',
+      maxWidth: '80vw',
+      backgroundColor: '#101318',
+      margin: 0,
+    }}
+  >
+    <nav className="flex items-center justify-between flex-wrap p-6">
+      <div className="flex-1" />
+      <div>
+        <MagicBell {...args}>
+          {(props) => (
+            <FloatingNotificationInbox
+              onAllRead={onAllRead}
+              onNotificationClick={onNotificationClick}
+              height={600}
+              {...props}
+            />
+          )}
+        </MagicBell>
+      </div>
+    </nav>
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  apiKey: 'df24a28e8921181f6c4220fc306ba76701592d21',
+  userEmail: 'josue@supportbee.com',
+  userKey: 'pvorWv0ff2MvYFNyadwOLmFzTZnT1LCFxzTELAULYT4=',
+  theme: {
+    icon: { borderColor: 'white' },
+    header: {
+      backgroundColor: '#FAD776',
+      backgroundOpacity: 0,
+      borderRadius: '4px',
+      textColor: 'white',
+    },
+    footer: {
+      backgroundColor: '#333A40',
+      backgroundOpacity: 1,
+      borderRadius: '4px',
+      textColor: 'white',
+    },
+    container: { backgroundColor: '#333A40', textColor: 'white' },
+    notification: {
+      default: { backgroundColor: '#333A40', borderRadius: '4px', textColor: 'white' },
+      unread: {
+        backgroundColor: '#212328',
+        backgroundOpacity: 0.75,
+        borderRadius: '4px',
+        textColor: 'white',
+      },
+      unseen: {
+        backgroundColor: '#009FF6',
+        backgroundOpacity: 0.2,
+        borderRadius: '4px',
+        textColor: 'white',
+      },
+    },
+  },
+};
---
+++ packages/react/stories/TailwindIntegration.stories.tsx
@@ -0,0 +1,58 @@
+import { INotification } from '@magicbell/react-headless';
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import FloatingNotificationInbox from '../src/components/FloatingNotificationInbox';
+import MagicBell from '../src/components/MagicBell';
+
+interface IStory {
+  handleAllRead: () => void;
+  onNotificationClick: (notification: INotification) => void;
+}
+
+export default {
+  title: 'Examples/Tailwind integration',
+  component: MagicBell,
+  argTypes: {
+    handleAllRead: { action: 'handleAllRead' },
+    onNotificationClick: { action: 'onNotificationClick' },
+  },
+} as Meta;
+
+const Template: Story<IStory> = ({ handleAllRead, onNotificationClick }: IStory) => {
+  return (
+    <div className="max-w-5xl w-screen">
+      <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+        <div className="flex-1">
+          <a className="text-blue-100 hover:text-white hover:border-white" href="/#">
+            Home
+          </a>
+        </div>
+        <div>
+          <MagicBell
+            apiKey="df24a28e8921181f6c4220fc306ba76701592d21"
+            userEmail="josue@supportbee.com"
+            theme={{
+              header: { borderRadius: '2px' },
+              footer: { borderRadius: '2px' },
+            }}
+          >
+            {(props) => (
+              <FloatingNotificationInbox
+                onNotificationClick={onNotificationClick}
+                onAllRead={handleAllRead}
+                height={450}
+                {...props}
+              />
+            )}
+          </MagicBell>
+        </div>
+      </nav>
+      <div className="bg-gray-100 h-96">
+        <h1 className="py-20 text-center text-2xl">Welcome to my site</h1>
+      </div>
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {};
