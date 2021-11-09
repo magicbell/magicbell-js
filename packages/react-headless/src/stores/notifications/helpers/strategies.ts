06e2af2a603b25760607ee46ce3d61658cd58a97
Josue Montano <josuemontanoa@gmail.com>
2021-11-09 00:46:10 -0400

    chore: define strategy to compare notificattions against an object

---
+++ packages/react-headless/src/stores/notifications/helpers/strategies.ts
@@ -0,0 +1,36 @@
+import isNil from 'ramda/src/isNil';
+import { IRemoteNotification } from '../../../types';
+import { IStrategyComparator } from '../../../types/INotificationStore';
+
+function eq(value, other) {
+  return value === other || (value !== value && other !== other);
+}
+
+/**
+ * Check if a notification satisfies all conditions of the given `context`. It
+ * Uses equal to compare.
+ *
+ * @param notification Notification to test
+ * @param context Set of rules to test the notification against
+ * @param comparator Function used to compare notification attributes and context values
+ */
+export function compareAttributeStrategy(
+  notification: IRemoteNotification,
+  context,
+  comparator: IStrategyComparator = eq,
+) {
+  const diff: string[] = [];
+
+  Object.keys(context).forEach((attr) => {
+    const conditionValue = context[attr];
+
+    if (
+      (attr === 'read' && !comparator(!isNil(notification.readAt), conditionValue)) ||
+      (attr === 'seen' && !comparator(!isNil(notification.seenAt), conditionValue)) ||
+      (Object.hasOwnProperty.call(notification, attr) && !comparator(notification[attr], conditionValue))
+    )
+      diff.push(attr);
+  });
+
+  return { result: diff.length === 0, delta: diff };
+}
