0ebd746d838c93a2a328f32bb6c6f4efa9e004da
Stephan Meijer <stephan.meijer@gmail.com>
2021-11-26 11:33:44 +0100

    chore: add react-hooks to eslint config (#5)

--- packages/react-headless/.eslintrc.js
+++ packages/react-headless/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['prettier', 'plugin:prettier/recommended'],
+  extends: ['prettier', 'plugin:prettier/recommended', 'plugin:react-hooks/recommended'],
   plugins: ['@typescript-eslint'],
 };
