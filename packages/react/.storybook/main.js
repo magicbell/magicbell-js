97a1e355def9ded26010c7501022e29720d106a6
Josue Montano <josuemontanoa@gmail.com>
2021-12-10 08:29:55 -0500

    chore: set static dir

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  staticDirs: ['../public'],
   babel: async (options) => {
     return {
       ...options,
