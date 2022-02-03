c2989d1c75e50fed7fcd4dfedb5c5b6191aea70e
Josue Montano <josuemontanoa@gmail.com>
2022-02-03 17:35:57 -0400

    chore: expor the FloatingInboxContainer component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,32 +1,6 @@
 import MagicBell from './components/MagicBell';
 export default MagicBell;
 
-export { default as Badge } from './components/Badge';
-export { default as Bell } from './components/Bell';
-export {
-  default as ClickableNotification,
-  StyledNotificationContainer,
-} from './components/ClickableNotification';
-export { EnablePushNotificationsButton } from './components/EnablePushNotificationsBanner';
-export { default as FloatingNotificationInbox } from './components/FloatingNotificationInbox';
-export { default as FloatingNotificationInboxArrow } from './components/FloatingNotificationInbox/Arrow';
-export { default as Footer } from './components/Footer';
-export { default as Header } from './components/Header';
-export { default as MagicBellProvider } from './components/MagicBellProvider';
-export { default as NotificationContent } from './components/NotificationContent';
-export { default as NotificationInbox } from './components/NotificationInbox';
-export { default as NotificationList } from './components/NotificationList';
-export { default as NotificationState } from './components/NotificationState';
-export { default as NotificationPreferences } from './components/UserPreferencesPanel/PreferencesCategories';
-export type { PopoverPlacement } from './components/Popover';
-export { default as Popover } from './components/Popover';
-export { default as PushNotificationsSubscriber } from './components/PushNotificationsSubscriber';
-export { default as Timestamp } from './components/Timestamp';
-export { default as MagicBellContext, useMagicBellContext } from './context/MagicBellContext';
-export { default as MagicBellThemeContext, useTheme } from './context/MagicBellThemeContext';
-export type { IMagicBellTheme } from './context/Theme';
-export { defaultTheme as defaultMagicBellTheme } from './context/Theme';
-export { darken, toRGBA } from './lib/color';
 export {
   clientSettings,
   eventAggregator,
@@ -44,3 +18,30 @@ export {
   useNotificationStoresCollection,
   useNotificationUnmount,
 } from '@magicbell/react-headless';
+export { default as Badge } from './components/Badge';
+export { default as Bell } from './components/Bell';
+export {
+  default as ClickableNotification,
+  StyledNotificationContainer,
+} from './components/ClickableNotification';
+export { EnablePushNotificationsButton } from './components/EnablePushNotificationsBanner';
+export { default as FloatingNotificationInbox } from './components/FloatingNotificationInbox';
+export { default as FloatingNotificationInboxArrow } from './components/FloatingNotificationInbox/Arrow';
+export { default as FloatingInboxContainer } from './components/FloatingNotificationInbox/FloatingInboxContainer';
+export { default as Footer } from './components/Footer';
+export { default as Header } from './components/Header';
+export { default as MagicBellProvider } from './components/MagicBellProvider';
+export { default as NotificationContent } from './components/NotificationContent';
+export { default as NotificationInbox } from './components/NotificationInbox';
+export { default as NotificationList } from './components/NotificationList';
+export { default as NotificationState } from './components/NotificationState';
+export { default as Popover } from './components/Popover';
+export type { PopoverPlacement } from './components/Popover';
+export { default as PushNotificationsSubscriber } from './components/PushNotificationsSubscriber';
+export { default as Timestamp } from './components/Timestamp';
+export { default as NotificationPreferences } from './components/UserPreferencesPanel/PreferencesCategories';
+export { default as MagicBellContext, useMagicBellContext } from './context/MagicBellContext';
+export { default as MagicBellThemeContext, useTheme } from './context/MagicBellThemeContext';
+export { defaultTheme as defaultMagicBellTheme } from './context/Theme';
+export type { IMagicBellTheme } from './context/Theme';
+export { darken, toRGBA } from './lib/color';
