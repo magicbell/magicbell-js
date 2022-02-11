4bcf611694b8121183ee79b4e8574b2579be2497
Stephan Meijer <stephan.meijer@gmail.com>
2022-02-11 15:13:35 +0100

    feat: add redirects

--- packages/playground/src/pages/index.tsx
+++ packages/playground/src/pages/index.tsx
@@ -1,11 +1,8 @@
 import { handle, redirect } from 'next-runtime';
 
-import { getAllExamples } from '~/lib/utils';
-
 export const getServerSideProps = handle({
   async get() {
-    const examples = await getAllExamples();
-    return redirect(`/example/${examples[0].slug}`);
+    return redirect(`/example/react`);
   },
 });
 
