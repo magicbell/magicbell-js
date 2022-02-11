ce5d9894dd14d50a409d95ec721f61366b1369ed
Stephan Meijer <stephan.meijer@gmail.com>
2022-02-11 15:24:16 +0100

    chore: move examples to root

--- packages/playground/src/pages/index.tsx
+++ packages/playground/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { handle, redirect } from 'next-runtime';
 
 export const getServerSideProps = handle({
   async get() {
-    return redirect(`/example/react`);
+    return redirect(`/react`);
   },
 });
 
