5b60e9921bf4fdc3c5d4050e407104ee551b89ea
Stephan Meijer <stephan.meijer@gmail.com>
2022-02-15 11:14:58 +0100

    feat: use custom index.html

--- packages/playground/src/lib/utils.ts
+++ packages/playground/src/lib/utils.ts
@@ -32,6 +32,41 @@ export async function getAllExamples(): Promise<ExampleConfig[]> {
   );
 }
 
+function getIndexHtml(template: SandpackPredefinedTemplate) {
+  const body =
+    template === 'vanilla-ts'
+      ? '<div id="app"></div><script src="src/index.js"></script>'
+      : template === 'react-ts'
+      ? '<div id="root"></div>'
+      : template === 'angular'
+      ? '<app-root></app-root>'
+      : template === 'vue'
+      ? '<div id="app"></div>'
+      : template === 'vue3'
+      ? '<div id="app"></div>'
+      : '';
+
+  return `<!doctype html>
+<html lang="en">
+            
+<head>
+    <meta charset="utf-8">
+    <title>MagicBell Playground</title>
+    <base href="/">
+            
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+            
+<body>
+  <div class="inline-block">
+    ${body}
+  </div>
+</body>
+            
+</html>`;
+}
+
 export async function getFilesForExample(
   folder: string,
 ): Promise<Record<string, { code: string }>> {
@@ -80,6 +115,15 @@ function rename(current: string, next: string) {
   };
 }
 
+function add(name: string, content: string, options: Record<string, any> = {}) {
+  return (obj) => {
+    return {
+      ...obj,
+      [name]: { code: content, ...options },
+    };
+  };
+}
+
 /**
  * Rename files based on the used template. This allows us to have a uniform folder
  * structure in /examples, even while Sandpack templates do not. Note that this
@@ -98,18 +142,31 @@ export function reshapeForSandpack(
   delete newFiles['/package.json'];
 
   if (template === 'vanilla-ts') {
-    return rename('/index.ts', '/src/index.ts')(newFiles);
+    return pipe(
+      rename('/index.ts', '/src/index.ts'),
+      add('/index.html', getIndexHtml(template), { hidden: true }),
+    )(newFiles);
   } else if (template === 'react-ts') {
-    return rename('/app.tsx', '/App.tsx')(newFiles);
+    return pipe(
+      rename('/app.tsx', '/App.tsx'),
+      add('/public/index.html', getIndexHtml(template), { hidden: true }),
+    )(newFiles);
   } else if (template === 'angular') {
     return pipe(
       rename('/app.component.ts', '/src/app/app.component.ts'),
       rename('/app.component.html', '/src/app/app.component.html'),
+      add('/src/index.html', getIndexHtml(template), { hidden: true }),
     )(newFiles);
   } else if (template === 'vue') {
-    return pipe(rename('/app.vue', '/src/App.vue'))(newFiles);
+    return pipe(
+      rename('/app.vue', '/src/App.vue'),
+      add('/public/index.html', getIndexHtml(template), { hidden: true }),
+    )(newFiles);
   } else if (template === 'vue3') {
-    return pipe(rename('/app.vue', '/src/App.vue'))(newFiles);
+    return pipe(
+      rename('/app.vue', '/src/App.vue'),
+      add('/public/index.html', getIndexHtml(template), { hidden: true }),
+    )(newFiles);
   } else {
     return newFiles;
   }
