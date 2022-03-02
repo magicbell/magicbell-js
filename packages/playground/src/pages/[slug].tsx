435195434da7654ef4db1afe165475702ae877ed
Stephan Meijer <stephan.meijer@gmail.com>
2022-03-02 17:38:34 +0100

    feat: add social share/open graph info

--- packages/playground/src/pages/[slug].tsx
+++ packages/playground/src/pages/[slug].tsx
@@ -7,6 +7,7 @@ import { GetStaticPropsResult } from 'next';
 import { useRef } from 'react';
 
 import Navbar from '~/components/layout/navbar';
+import SocialHead from '~/components/layout/social-head';
 import Sandbox from '~/components/sandbox/sandbox';
 import Logo from '~/components/svg/logo';
 import SlackLogo from '~/components/svg/slack';
@@ -69,45 +70,57 @@ export default function Example({
   const contentRef = useRef();
   const height = useHeight(contentRef, 300);
 
+  const example = examples.find((x) => x.slug === slug);
+  const name = slug
+    .split('-')
+    .map((x) => x[0].toUpperCase() + x.slice(1))
+    .join(' ');
+
   return (
-    <div className="h-screen max-w-9xl px-8 pt-6 mx-auto flex flex-col">
-      <header className="relative">
-        <div className="flex w-full">
-          <Logo />
-        </div>
-        <Navbar examples={examples} currentPath={slug} />
-      </header>
+    <>
+      <SocialHead
+        title={`${name} | MagicBell Playground`}
+        description={example.description}
+      />
+      <div className="h-screen max-w-9xl px-8 pt-6 mx-auto flex flex-col">
+        <header className="relative">
+          <div className="flex w-full">
+            <Logo />
+          </div>
+          <Navbar examples={examples} currentPath={slug} />
+        </header>
 
-      <div className="mt-6 h-full overflow-hidden relative">
-        <div className="h-full" ref={contentRef}>
-          <Sandbox
-            key={slug} // remount component on page change
-            template={template}
-            define={{
-              MAGICBELL_API_KEY: 'e1f70f214b0be42a8efde915af39feacc956b06b',
-              MAGICBELL_USER_EMAIL: 'person@example.com',
-              MAGICBELL_USER_KEY: '...',
-            }}
-            setup={{ dependencies }}
-            files={files}
-            height={`${height - 40}px`}
-          />
+        <div className="mt-6 h-full overflow-hidden relative">
+          <div className="h-full" ref={contentRef}>
+            <Sandbox
+              key={slug} // remount component on page change
+              template={template}
+              define={{
+                MAGICBELL_API_KEY: 'e1f70f214b0be42a8efde915af39feacc956b06b',
+                MAGICBELL_USER_EMAIL: 'person@example.com',
+                MAGICBELL_USER_KEY: '...',
+              }}
+              setup={{ dependencies }}
+              files={files}
+              height={`${height - 40}px`}
+            />
+          </div>
         </div>
+
+        <footer className="mt-6 mb-12 flex-row justify-end space-x-6 text-xs">
+          <a href="https://magicbell.com/docs" className="flex-row fg-body">
+            <FileTextIcon className="mr-2" /> Docs
+          </a>
+          <a
+            href="https://join.slack.com/t/magicbell-community/shared_invite/zt-trh6yi84-~jtPqNikvC1m3My_p0WUqw"
+            className="flex-row fg-body"
+            target="_blank"
+            rel="noopener nofollow noreferrer"
+          >
+            <SlackLogo className="mr-2" /> Slack
+          </a>
+        </footer>
       </div>
-
-      <footer className="mt-6 mb-12 flex-row justify-end space-x-6 text-xs">
-        <a href="https://magicbell.com/docs" className="flex-row fg-body">
-          <FileTextIcon className="mr-2" /> Docs
-        </a>
-        <a
-          href="https://join.slack.com/t/magicbell-community/shared_invite/zt-trh6yi84-~jtPqNikvC1m3My_p0WUqw"
-          className="flex-row fg-body"
-          target="_blank"
-          rel="noopener nofollow noreferrer"
-        >
-          <SlackLogo className="mr-2" /> Slack
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
