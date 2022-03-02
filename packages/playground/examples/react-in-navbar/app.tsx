6bddd14f2eb2b4c2b1a8757ff3c8feb8e18b4a0b
Stephan Meijer <stephan.meijer@gmail.com>
2022-03-02 16:27:41 +0100

    chore: cleanup examples

--- packages/playground/examples/react-in-navbar/app.tsx
+++ packages/playground/examples/react-in-navbar/app.tsx
@@ -6,19 +6,19 @@ import MagicBell, {
 
 export default function Index() {
   return (
-    <nav className="flex justify-between bg-white p-4 m-4 rounded shadow-lg">
+    <nav className="flex justify-between bg-white p-4 m-4 rounded shadow-lg fixed left-0 right-0 top-0">
       <ul className="flex space-x-8">
         <li>home</li>
         <li>about</li>
       </ul>
 
       <MagicBell
-        apiKey="9b110461aa11a33b41cceb346dcfe030af14f39d"
-        userEmail="stephan.meijer@gmail.com"
-        userKey="71qj/ah4otyHvDJnRVAFMvD71SuRjGtogzOcWOVUblM="
+        apiKey="__MAGICBELL_API_KEY__"
+        userEmail="__MAGICBELL_USER_EMAIL__"
+        userKey="__MAGICBELL_USER_KEY__"
         theme={{ icon: { borderColor: '#000' } }}
       >
-        {(props) => <FloatingNotificationInbox height={300} {...props} />}
+        {(props) => <FloatingNotificationInbox height={450} {...props} />}
       </MagicBell>
     </nav>
   );
