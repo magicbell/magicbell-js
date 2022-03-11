1e6132b8a906a8bbd8f030875fdb8dd150bdf1da
Eric Hosick <erichosick@gmail.com>
2022-03-11 12:48:02 -0800

    [chore]: Cleanup and CleanupAll  (#60)
    * [chore]: provide cleanup function
    
    * Chore: Docs + Cleanup
    
    [chore]: Add docs for clean and cleanAll methods.
    [chore]: Add more information to documentation
    [chore]: Split cleanup into two functions: cleanup a specific target and cleanupAll targets that have a MagicBell widget embedded in them.
    
    * [chore]: remoe unnecessary comment

--- packages/embeddable/src/index.tsx
+++ packages/embeddable/src/index.tsx
@@ -1,3 +1,8 @@
+// NOTE: This is required to run react Developer Tools. Loading it conditionally
+// doesn't seem to be possible as it seems that Cypress does not recognize
+// require.
+import 'preact/debug';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Widget, { WidgetProps } from './components/Widget';
@@ -16,38 +21,78 @@ function validateParams(options) {
 }
 
 /**
- * Function that renders the widget if all options are valid.
+ * This maintains a list of active MagicBell widgets on the page. This is
+ * primarily used as a way to enable us to cleanup all the active widgets on
+ * the page.
+ * NOTE: The approach of maintaining active widgets internally like this was
+ * done to maintain backwards compatability with renderWidget.
+ */
+const activeWidgets: HTMLElement[] = [];
+
+/**
+ * Function that renders a MagicBell widget if all options are valid.
  *
  * @example
- * renderWidget({ userEmail, apiKey, target: document.getElementById('test') })
+ * renderWidget(document.getElementById('mbWidget'), { userEmail, apiKey })
  */
 export function renderWidget(target: HTMLElement, options: WidgetProps): MagicBellEventEmitter {
   validateParams({ target, ...options });
   ReactDOM.render(<Widget {...options} />, target);
-
+  activeWidgets.push(target);
   return emitter;
 }
 
 /**
+ * Function that cleans up the one or more MagicBell widgets
+ * @param target The targetElement containing the magicbell widget to clean up.
+ * @example
+ * cleanup(document.getElementById('mbWidget')) // cleans up test widget
+ */
+export function cleanup(targetElement: HTMLElement) {
+  // NOTE: Don't need to remove this targetElement from the activeWidgets as
+  // calling unmountComponentAtNode is apparently idempotent.
+  ReactDOM.unmountComponentAtNode(targetElement);
+}
+
+/**
+ * Function that cleans up all MagicBell widgets
+ * @example
+ * cleanup() // cleans up all MagicBell widgets on the page
+ */
+export function cleanupAll() {
+  let activeTarget = activeWidgets.pop();
+  while (undefined !== activeTarget) {
+    ReactDOM.unmountComponentAtNode(activeTarget);
+    activeTarget = activeWidgets.pop();
+  }
+}
+
+/**
  * Function called when this module is imported. Iterates over
- * all items in the MagicBellObject's queue, if it exists.
+ * all items in the MagicBellObject's queue.
  *
- * The `render` action renders the MagicBell widget with the provided
- * options. This is the only supported action.
+ * Available queue actions:
+ * * render - The `render` action renders the MagicBell widget with the provided
+ * options. NOTE: Currently the only supported action.
  */
 function dequeue(window) {
-  let globalObject = window[window['MagicBellObject']];
-  const { q: queue } = globalObject || {};
+  const { q: queue } = window[window['MagicBellObject']] || {};
 
   if (queue) {
     queue.forEach((element) => {
       const [method, ...options] = Array.from(element);
 
       if (method.toLowerCase().trim() === 'render') {
-        globalObject = renderWidget(options[0], options[1]);
+        renderWidget(options[0], options[1]);
       }
     });
   }
 }
 
 dequeue(window);
+
+// For the use case where someone takes advantage of our MagcBell queue (see
+// dequeu above), we will automatically cleanup on a reload.
+window.addEventListener('beforeunload', () => {
+  cleanupAll();
+});
