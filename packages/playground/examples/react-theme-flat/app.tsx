a6af5576757661243cbd455148748a56266962af
Stephan Meijer <stephan.meijer@gmail.com>
2022-07-28 17:31:10 +0200

    chore: add theme examples

---
+++ packages/playground/examples/react-theme-flat/app.tsx
@@ -0,0 +1,17 @@
+import MagicBell, {
+  flatTheme,
+  FloatingNotificationInbox,
+} from '@magicbell/magicbell-react';
+
+export default function Index() {
+  return (
+    <MagicBell
+      apiKey="__MAGICBELL_API_KEY__"
+      userEmail="__MAGICBELL_USER_EMAIL__"
+      userKey="__MAGICBELL_USER_KEY__"
+      theme={flatTheme}
+    >
+      {(props) => <FloatingNotificationInbox height={450} {...props} />}
+    </MagicBell>
+  );
+}
