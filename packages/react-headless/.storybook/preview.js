9a5f5727a1c9afb3a7cb9c6445edbd6426db90f5
Stephan Meijer <stephan.meijer@gmail.com>
2022-08-25 13:19:05 +0200

    chore: register global vars for storybook

--- packages/react-headless/.storybook/preview.js
+++ packages/react-headless/.storybook/preview.js
@@ -1,5 +1,11 @@
 import 'twind/shim';
 
+const pkg = require('../package.json');
+
+global.__DEV__ = true;
+global['__PACKAGE_NAME__'] = pkg.name;
+global['__PACKAGE_VERSION__'] = pkg.version;
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
