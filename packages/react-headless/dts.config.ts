90d31fdd27f72cf0e28babb4176baa598a6606a0
Stephan Meijer <stephan.meijer@gmail.com>
2022-08-26 10:08:41 +0200

    chore: don't bundle all deps in UMD bundle

--- packages/react-headless/dts.config.ts
+++ packages/react-headless/dts.config.ts
@@ -4,29 +4,17 @@ import type { DtsConfig } from 'dts-cli';
 
 const pkg = require('./package.json');
 
-const umdExternals = new Set(['react', 'react-dom']);
-
 const config: DtsConfig = {
   rollup(config) {
-    const output = (Array.isArray(config.output) ? config.output[0] : config.output) || {};
-
     config.plugins = config.plugins || [];
 
-    // add replace op before sourcemaps are generated
-    const index = config.plugins.findIndex((x) => x && x.name === 'sourcemaps');
-    config.plugins.splice(
-      index,
-      0,
+    config.plugins.unshift(
       replace({
         __PACKAGE_NAME__: JSON.stringify(pkg.name),
         __PACKAGE_VERSION__: JSON.stringify(pkg.version),
         preventAssignment: true,
       }),
     );
-
-    if (output.format === 'umd') {
-      config.external = (id) => umdExternals.has(id.split('/')[0]);
-    }
 
     return config;
   },
