cdfa23a2e9f61470ca2789ad62280ba4d887ff8c
Stephan Meijer <stephan.meijer@gmail.com>
2022-09-09 20:33:25 +0200

    chore: remove unused code

--- packages/react/src/lib/window.ts
+++ packages/react/src/lib/window.ts
@@ -1,6 +1,4 @@
 /* eslint-disable no-restricted-globals */
-import { MutableRefObject, useEffect, useState } from 'react';
-import ResizeObserver from 'resize-observer-polyfill';
 
 /**
  * Open a centered popup window.
@@ -11,30 +9,4 @@ export function openWindow(url: string) {
   const top = (screen.height - 400) / 4;
   const left = (screen.width - 600) / 2;
   window.open(url, '', `width=600,height=400,scrollbars=no,top=${top},left=${left}`);
-}
-
-/**
- * Custom hook to observe the height of an HTML element.
- *
- * @param ref - the element to observe, either a ref object or html element
- * @param initialHeight - optional initial height
- */
-export function useHeight(ref: MutableRefObject<Element> | Element | null, initialHeight?: number) {
-  const [height, setHeight] = useState(initialHeight);
-
-  useEffect(() => {
-    if (!ref) return;
-
-    const target = ref instanceof Element ? ref : ref.current;
-    if (!target) return;
-
-    const observer = new ResizeObserver(([entry]) => {
-      setHeight(entry.contentRect.height);
-    });
-
-    observer.observe(target);
-    return () => observer.disconnect();
-  }, [ref, setHeight]);
-
-  return height;
 }
