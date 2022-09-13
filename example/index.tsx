975094ba75438ace07e820b0001f67fc603b5023
Stephan Meijer <stephan.meijer@gmail.com>
2022-09-13 15:48:17 +0200

    chore: cleanup

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,15 @@
 import 'react-app-polyfill/ie11';
+
+import MagicBell, { FloatingNotificationInbox } from '@magicbell/magicbell-react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '@magicbell/react';
-import '@magicbell/react/dist/styles.css';
 
 function App() {
   return (
-    <div>
-      <Thing />
+    <div id="target">
+      <MagicBell apiKey="api-key-here" userEmail="stephan@magicbell.io" userKey="...">
+        {(props) => <FloatingNotificationInbox height={450} {...props} />}
+      </MagicBell>{' '}
     </div>
   );
 }
--- example/vite.config.js
+++ example/vite.config.js
@@ -3,6 +3,9 @@ import ReactPlugin from 'vite-preset-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
   plugins: [
     ReactPlugin({
       injectReact: false,
