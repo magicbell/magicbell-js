e2f3785d8a540d5b5bf9574e22786e5155f33998
Stephan Meijer <stephan.meijer@gmail.com>
2022-09-29 19:34:53 +0200

    chore: don't build type defs in watch mode

--- scripts/vite/plugins/writeTypeDefs.js
+++ scripts/vite/plugins/writeTypeDefs.js
@@ -3,10 +3,15 @@ import { execSync } from 'child_process';
 import { pkg } from '../settings.js';
 
 export async function writeTypeDefs() {
+  let watch = false;
+
   return {
     name: 'generate types',
+    config(config) {
+      watch = config.build.watch;
+    },
     async buildEnd(errors) {
-      if (errors) return;
+      if (watch || errors) return;
       if (!pkg.typings) {
         this.warn(`package ${pkg.name} does not export any types. Set package.json#typings if this is a mistake.`);
         return;
