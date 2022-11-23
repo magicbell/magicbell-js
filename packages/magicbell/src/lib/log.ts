de308eb992726b54c4053649ab46abd90c1d53ec
Stephan Meijer <stephan.meijer@gmail.com>
2022-11-23 16:53:53 +0100

    chore: print curl command for failed requests in debug mode

--- packages/magicbell/src/lib/log.ts
+++ packages/magicbell/src/lib/log.ts
@@ -1,6 +1,9 @@
+import { AxiosRequestConfig } from 'axios';
+
 const colors = {
   reset: '\x1b[0m',
   magenta: '\x1b[35m',
+  red: '\x1b[31m',
 };
 
 export function emitWarning(message) {
@@ -20,4 +23,32 @@ export class Logger {
     // eslint-disable-next-line no-console
     console.log(`${colors.reset}${colors.magenta}magicbell:${colors.reset} ${message}`);
   }
+
+  error(message) {
+    if (!this.active) return;
+
+    // eslint-disable-next-line no-console
+    console.error(`${colors.reset}${colors.red}magicbell:${colors.reset} ${message}`);
+  }
+}
+
+function mask(str: string) {
+  if (__DEV__) return str;
+  return `${str.slice(0, 4)}…${str.slice(-4)}`;
+}
+
+const secrets = /secret|token|key|password/i;
+function isSecret(key: string) {
+  return secrets.test(key);
+}
+
+export function toCurl({ method, baseURL, url, data, headers }: AxiosRequestConfig) {
+  return [
+    `curl -X ${method.toUpperCase()}`,
+    `${baseURL}/${url.replace(/^\//, '')}`,
+    Object.entries(headers)
+      .map(([key, value]) => `-H '${key}: ${isSecret(key) ? mask(String(value)) : value}'`)
+      .join(' '),
+    data && `-d '${JSON.stringify(data)}'`,
+  ].join(' ');
 }
