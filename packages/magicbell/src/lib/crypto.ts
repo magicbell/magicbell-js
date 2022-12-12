c7bbaf5b6fed69aaf2cc966f42e2fcab0a92f856
Stephan Meijer <stephan.meijer@gmail.com>
2022-12-12 15:11:09 +0100

    fix: don't create hmac if required data is missing

--- packages/magicbell/src/lib/crypto.ts
+++ packages/magicbell/src/lib/crypto.ts
@@ -1,7 +1,11 @@
 import crypto from 'crypto';
 
-export function computeUserKey(apiSecret: string, userEmail: string) {
-  if (!apiSecret || !userEmail) return '';
+export function createHmac(secret: string, data: string) {
+  if (!secret || !data) return '';
 
-  return crypto.createHmac('sha256', apiSecret).update(userEmail).digest('base64');
+  if (!crypto || !crypto.createHmac) {
+    throw new Error('This method is not available in the browser. Please provide a userHmac.');
+  }
+
+  return crypto.createHmac('sha256', secret).update(data).digest('base64');
 }
