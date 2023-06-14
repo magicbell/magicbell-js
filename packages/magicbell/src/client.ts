66dae2ee1fb2a6cb043b8160f918dc1be1c0e0b7
Stephan Meijer <stephan.meijer@gmail.com>
2023-06-14 16:52:22 +0200

    feat: add metrics resource (#134)

--- packages/magicbell/src/client.ts
+++ packages/magicbell/src/client.ts
@@ -10,6 +10,7 @@ import { createListener } from './listen';
 import { isOptionsHash } from './options';
 import { Broadcasts } from './resources/broadcasts';
 import { Imports } from './resources/imports';
+import { Metrics } from './resources/metrics';
 import { NotificationPreferences } from './resources/notification-preferences';
 import { Notifications } from './resources/notifications';
 import { PushSubscriptions } from './resources/push-subscriptions';
@@ -43,6 +44,7 @@ export class Client {
 
   broadcasts = new Broadcasts(this);
   imports = new Imports(this);
+  metrics = new Metrics(this);
   notificationPreferences = new NotificationPreferences(this);
   notifications = new Notifications(this);
   pushSubscriptions = new PushSubscriptions(this);
