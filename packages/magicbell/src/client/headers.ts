5f90e5d88d2a3d6fd07d06b62938d2b642b75d1d
Stephan Meijer <stephan.meijer@gmail.com>
2023-09-19 15:58:35 +0200

    chore: move uuid helper function to improve tree shaking

--- packages/magicbell/src/client/headers.ts
+++ packages/magicbell/src/client/headers.ts
@@ -1,8 +1,15 @@
 import { deleteEmptyHeaders } from 'fetch-addons';
 import type { Hooks } from 'ky';
 
-import { uuid4 } from '../crypto';
 import { ClientOptions } from './types';
+
+function uuid4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 function getDefaultIdempotencyKey(method: string, maxRetries: number) {
   if (method.toUpperCase() !== 'POST' || maxRetries === 0) return;
