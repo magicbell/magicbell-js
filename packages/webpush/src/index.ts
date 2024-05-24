7bea88f45389329261843d7b2e0fb8d06fe62078
Stephan Meijer <stephan.meijer@gmail.com>
2024-05-24 13:05:08 +0200

    feat: use magicbell integrations (#283)

--- packages/webpush/src/index.ts
+++ packages/webpush/src/index.ts
@@ -1,203 +1,124 @@
-type RequestOptions = {
-  token: string;
-  project: string;
-  baseURL: string;
+import { ChannelToken, ClientOptions, IntegrationClient } from './internal/client';
+
+export type Installation = {
+  public_key: string;
+  auth_token: string;
 };
 
-type Config = {
-  user: { id: string; email?: string | null; external_id?: string | null; hmac?: string | null };
-  project: { id: number; subdomain: string; api_key: string; vapid_public_key: string };
-  website_push_id: string;
+export type Token = ChannelToken & {
+  endpoint?: string;
+  keys?: Record<string, string>;
 };
 
-type SubscribeOptions = {
-  token: string;
-  project: string;
-  serviceWorkerPath?: string;
-  host?: string;
-};
+export type WebPushClientOptions = ClientOptions & { serviceWorkerPath?: string };
 
-type Subscription = {
-  created_at: string;
-  device_token: string;
-  discarded_at: string | null;
-  id: string;
-  platform: string;
-  user_id: string;
-};
+export class WebPushClient extends IntegrationClient<Installation, Token> {
+  #serviceWorkerPath: string;
 
-type AuthTokenOptions = {
-  host?: string;
-  apiKey: string;
-} & ({ userEmail: string; userHmac?: string } | { userExternalId: string; userHmac?: string });
+  constructor(options: WebPushClientOptions) {
+    super('web_push', options);
+    this.#serviceWorkerPath = options.serviceWorkerPath || '/sw.js';
+  }
 
-let _config: Config | null = null;
-let _cacheKey = '';
+  /**
+   * Gets the registered service worker, and attempts to create one if no registration exists
+   */
+  async #getServiceWorker() {
+    return registerServiceWorker(this.#serviceWorkerPath);
+  }
 
-const DEFAULT_HOST = 'https://api.magicbell.com';
+  /**
+   * Checks if the current user has an active push subscription that is registered by MagicBell.
+   */
+  async isSubscribed(): Promise<boolean> {
+    const subscriptions = await this.getTokens();
+    const registration = await this.#getServiceWorker();
+    const activeSubscription = await registration?.pushManager?.getSubscription();
 
-const api = {
-  async getAuthToken(options: AuthTokenOptions): Promise<Pick<SubscribeOptions, 'token' | 'project' | 'host'>> {
-    const headers: Record<string, string> = {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      'x-magicbell-api-key': options.apiKey,
-    };
-
-    if ('userExternalId' in options && options.userExternalId) {
-      headers['x-magicbell-user-external-id'] = options.userExternalId;
-    } else if ('userEmail' in options && options.userEmail) {
-      headers['x-magicbell-user-email'] = options.userEmail;
-    } else {
-      throw new Error('Missing user email or external ID');
-    }
-
-    if (options.userHmac) {
-      headers['x-magicbell-user-hmac'] = options.userHmac;
-    }
-
-    return fetch(`${options.host || DEFAULT_HOST}/config`, {
-      method: 'GET',
-      headers: headers,
-    })
-      .then((x) => x.json())
-      .then((x) => {
-        const url = new URL(x.web_push_notifications.subscribe_url);
-
-        return {
-          host: options.host || DEFAULT_HOST,
-          token: url.searchParams.get('access_token') || null,
-          project: url.searchParams.get('project') || null,
-        };
+    if (!activeSubscription?.endpoint) return false;
+    return subscriptions
+      .filter((x) => !x.discarded_at)
+      .some((subscription) => {
+        return subscription.endpoint === activeSubscription.endpoint;
       });
-  },
+  }
 
-  async getConfig({ token, project, baseURL }: RequestOptions) {
-    const cacheKey = [token, project, baseURL].join('-');
-    if (_config && _cacheKey === cacheKey) return _config;
-
-    return fetch(`${baseURL}/web_push_subscriptions?access_token=${token}&project=${project}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    })
-      .then((x) => x.json() as Promise<{ push_subscription: Config }>)
-      .then((x) => {
-        _config = x.push_subscription;
-        _cacheKey = cacheKey;
-        return _config;
-      });
-  },
-
-  async getSubscriptions({ token, project, baseURL }: RequestOptions): Promise<Array<Subscription>> {
-    const config = await this.getConfig({ token, project, baseURL });
-    if (!config.project.api_key) throw new Error('Missing API key');
-    const headers: Record<string, string> = {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      'x-magicbell-api-key': config.project.api_key,
-    };
-    if (config.user.email) {
-      headers['x-magicbell-user-email'] = config.user.email;
-    } else if (config.user.external_id) {
-      headers['x-magicbell-user-external-id'] = config.user.external_id;
+  /**
+   * Request permission to send push notifications and post the subscription to the MagicBell API.
+   */
+  async subscribe(): Promise<void> {
+    if (!isSupported()) {
+      throw new Error('Push notifications are not supported in this browser');
     }
-    if (config.user.hmac) {
-      headers['x-magicbell-user-hmac'] = config.user.hmac;
+
+    const registration = await this.#getServiceWorker();
+    if (!registration?.pushManager) {
+      throw new Error('Push notifications are not supported in this browser');
     }
-    return fetch(`${baseURL}/push_subscriptions`, {
-      method: 'GET',
-      headers,
-    })
-      .then((result) => result.json())
-      .then((result) => result?.push_subscriptions || []);
-  },
 
-  async updateSubscription({ token, project, baseURL }: RequestOptions, subscription: PushSubscriptionJSON) {
-    return fetch(`${baseURL}/web_push_subscriptions?access_token=${token}&project=${project}`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        accept: 'application/json',
-      },
-      body: JSON.stringify({
-        web_push_subscription: {
-          data: subscription,
-        },
-      }),
-    })
-      .then((x) => x.json() as Promise<{ web_push_subscription: { id: string } }>)
-      .then((x) => x.web_push_subscription);
-  },
-};
+    // remove active subscription if there's any
+    await this.unsubscribe();
 
+    const config = await this.startInstallation();
+
+    // strip the base64 padding, it's either that or convert to uint8array
+    const applicationServerKey = config.public_key.replace(/=/g, '');
+
+    const subscription = await registration.pushManager
+      .subscribe({ userVisibleOnly: true, applicationServerKey })
+      .then((x) => x.toJSON());
+
+    if (!('endpoint' in subscription)) {
+      throw new Error('Failed to subscribe to push notifications, browser did not return an subscription endpoint.');
+    }
+
+    await this.createToken(subscription);
+  }
+
+  async unsubscribe(): Promise<void> {
+    const registration = await this.#getServiceWorker();
+    if (!registration?.pushManager) return;
+
+    const activeSubscription = await registration.pushManager.getSubscription();
+    if (!activeSubscription) return;
+
+    const endpoint = activeSubscription.endpoint;
+
+    // we're just purging an old subscription, MagicBell backend will also do that on the next broadcast
+    // so any error here, is non-critical
+    void this.getTokens()
+      .then((tokens: Token[]) => {
+        const token = tokens.find((token) => token.endpoint === endpoint);
+        if (!token) return;
+        return this.deleteToken(token.id);
+      })
+      .catch(() => void 0);
+
+    await activeSubscription.unsubscribe().catch(() => void 0);
+  }
+
+  async getAuthToken(): Promise<string> {
+    const installation = await this.startInstallation();
+    return installation.auth_token;
+  }
+}
+
+/**
+ * Check if service workers and push notifications are supported in this browser
+ */
 export function isSupported() {
   if (typeof window === 'undefined' || typeof navigator === 'undefined') return false;
   return 'PushManager' in window && 'serviceWorker' in navigator;
 }
 
-export async function prefetchConfig(options: SubscribeOptions) {
-  await api.getConfig({ ...options, baseURL: options.host || DEFAULT_HOST });
-}
+/**
+ * Gets the registered service worker, and attempts to create one if no registration exists
+ */
+export async function registerServiceWorker(options?: { path?: string } | string) {
+  const scriptUrl = (typeof options === 'string' ? options : options?.path) || '/sw.js';
 
-export async function registerServiceWorker({ path = '/sw.js' }: { path?: string } = {}) {
   // don't register a service-worker if there's already one
   if (navigator.serviceWorker.controller) return navigator.serviceWorker.ready;
-  await navigator.serviceWorker.register(path);
+  await navigator.serviceWorker.register(scriptUrl);
   return navigator.serviceWorker.ready;
-}
-
-/**
- * Checks if the current user has an active push subscription that is registered by MagicBell.
- */
-export async function isSubscribed(options: SubscribeOptions): Promise<boolean> {
-  const baseURL = options.host || DEFAULT_HOST;
-  const subscriptions = await api.getSubscriptions({ ...options, baseURL });
-  const registration = await registerServiceWorker({ path: options.serviceWorkerPath });
-  const activeSubscription = await registration?.pushManager?.getSubscription();
-
-  if (!activeSubscription?.endpoint) return false;
-  return subscriptions.some((subscription) => subscription.device_token === activeSubscription.endpoint);
-}
-
-/**
- * Request permission to send push notifications and post the subscription to the MagicBell API.
- */
-export async function subscribe(options: SubscribeOptions) {
-  if (!isSupported()) {
-    throw new Error('Push notifications are not supported in this browser');
-  }
-
-  const baseURL = options.host || DEFAULT_HOST;
-  const config = await api.getConfig({ ...options, baseURL });
-  const registration = await registerServiceWorker({ path: options.serviceWorkerPath });
-
-  if (!registration?.pushManager) {
-    throw new Error('Push notifications are not supported in this browser');
-  }
-
-  // remove active subscription if there's any
-  const activeSubscription = await registration.pushManager.getSubscription();
-  if (activeSubscription) {
-    await activeSubscription.unsubscribe().catch(() => void 0);
-  }
-
-  // strip the base64 padding, it's either that or convert to uint8array
-  const applicationServerKey = config.project.vapid_public_key.replace(/=/g, '');
-
-  const subscription = await registration.pushManager
-    .subscribe({ userVisibleOnly: true, applicationServerKey })
-    .then((x) => x.toJSON());
-
-  if (!('endpoint' in subscription)) {
-    throw new Error('Failed to subscribe to push notifications, browser did not return an subscription endpoint.');
-  }
-
-  await api.updateSubscription({ ...options, baseURL }, subscription);
-}
-
-export async function getAuthToken(options: AuthTokenOptions) {
-  return api.getAuthToken(options);
 }
