b28425fa695d69addcbe390d1a921730d95a7e5c
Stephan Meijer <stephan.meijer@gmail.com>
2024-07-08 17:34:35 +0200

    chore: fix tsconfigs

--- packages/react/src/components/MagicBellProvider/MagicBellChildrenWrapper.tsx
+++ packages/react/src/components/MagicBellProvider/MagicBellChildrenWrapper.tsx
@@ -7,12 +7,14 @@ import MagicBellProvider, { MagicBellProviderProps } from './MagicBellProvider';
  * Component that conditionally wraps the children with a MagicBellProvider
  * unless one is defined already.
  */
-export default function MagicBellChildrenWrapper({ children, apiKey, ...settings }: MagicBellProviderProps) {
+export default function MagicBellChildrenWrapper({ children, apiKey, ...settings }: Partial<MagicBellProviderProps>) {
   const currentContext = useContext(CurrentProviderContext);
 
   if (currentContext === NO_PROVIDER) {
     if (!apiKey) return null;
     return (
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       <MagicBellProvider apiKey={apiKey} {...settings}>
         {children}
       </MagicBellProvider>
