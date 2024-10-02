fb557ed7fbb4184f9e24a30f7f53835d0ab9ab36
Stephan Meijer <stephan.meijer@gmail.com>
2024-10-02 14:38:21 +0200

    chore: drop tippy polyfill test

--- packages/react/tests/bundle/commonjs.cjs
+++ packages/react/tests/bundle/commonjs.cjs
@@ -1,9 +1,5 @@
 const assert = require('node:assert');
 const { InfiniteScroll } = require('../../dist/commonjs/polyfills/infinite-scroll-module.js');
-const { Tippy } = require('../../dist/commonjs/polyfills/tippy-module.js');
 
 assert.equal(typeof InfiniteScroll, 'function');
 assert.equal(typeof InfiniteScroll.getDerivedStateFromProps, 'function');
-
-assert.equal(typeof Tippy, 'function');
-assert.equal(typeof Tippy.defaultProps, 'object');
--- packages/react/tests/bundle/esm.mjs
+++ packages/react/tests/bundle/esm.mjs
@@ -1,9 +1,5 @@
 import assert from 'node:assert';
 import { InfiniteScroll } from '../../dist/commonjs/polyfills/infinite-scroll-module.js';
-import { Tippy } from '../../dist/esm/polyfills/tippy-module.js';
 
 assert.equal(typeof InfiniteScroll, 'function');
 assert.equal(typeof InfiniteScroll.getDerivedStateFromProps, 'function');
-
-assert.equal(typeof Tippy, 'function');
-assert.equal(typeof Tippy.defaultProps, 'object');
