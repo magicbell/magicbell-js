d657e30452fa1a23d30f36673ce5b93b18fe2f6f
Stephan Meijer <stephan.meijer@gmail.com>
2024-10-04 21:51:32 +0200

    chore: extract service-worker to its own package (#401)

--- packages/embeddable/vite.config.js
+++ packages/embeddable/vite.config.js
@@ -6,26 +6,17 @@ import baseConfig from '../../scripts/vite/vite.config.js';
 export default defineConfig(async (configEnv) => {
   const base = await baseConfig(configEnv);
 
-  if (process.env.ENTRY === 'sw') {
-    base.build.outDir = 'dist/web-push-notifications';
-    base.build.lib = {
-      ...base.build.lib,
-      formats: ['cjs'],
-      fileName: () => `sw.min.js`,
-    };
-  } else {
-    base.build.lib = {
-      ...base.build.lib,
-      formats: ['esm', 'umd'],
-      name: 'MagicBell',
-      fileName: (format) =>
-        createFilename({
-          name: 'magicbell',
-          format: format === 'umd' ? '' : format,
-          minify: shouldMinify,
-        }),
-    };
-  }
+  base.build.lib = {
+    ...base.build.lib,
+    formats: ['esm', 'umd'],
+    name: 'MagicBell',
+    fileName: (format) =>
+      createFilename({
+        name: 'magicbell',
+        format: format === 'umd' ? '' : format,
+        minify: shouldMinify,
+      }),
+  };
 
   base.build.rollupOptions.external = false;
   base.resolve = {
